b1dee695bf288d77d3bd2f1613890aa3a678ada6
JamesOrdner <coasterguy123@gmail.com>
2017-12-04 15:38:13 -0800

    Now supports ingredient "synonyms"

--- DataProcessing/cleanIngredients.py
+++ DataProcessing/cleanIngredients.py
@@ -1,10 +1,10 @@
 import json
 
 def main():
-    with open('Brownies.json') as data_file:
+    with open('Lasagna.json') as data_file:
         data = json.load(data_file)
 
-        clean_names_file = open('Brownies_Ingredients_Clean.json', 'r')
+        clean_names_file = open('Lasagna_Ingredients_Clean_Full.json', 'r')
         clean_names = json.load(clean_names_file)
 
         # data is a dictionary with keys 'name', 'rating', 'reviews', and 'ingredients'.
@@ -13,13 +13,31 @@ def main():
         
         for i in range(0, len(data['name'])):
             for j in range(0, len(data['ingredients'][i])):
-                print('Ingredient name: ', match_ingredient(data['ingredients'][i][j], clean_names['ingredients']))
+                name = match_ingredient(data['ingredients'][i][j], clean_names)
+                if name is not '':
+                    print('Clean ingredient name: ',name) # Clean ingredient match here!
+
+def excluded(raw_ingredient, exclude_list):
+    for s in exclude_list:
+        if s in raw_ingredient.lower():
+            return True
+
+    return False
+
+def found_better_match(s_clean, raw_ingredient, best_match, exclude_list):
+    return s_clean in raw_ingredient.lower() and len(s_clean) > len(best_match) \
+       and not excluded(raw_ingredient, exclude_list)
 
 def match_ingredient(raw_ingredient, parsed_names):
     best_match = ''
-    for s in parsed_names:
-        if s in raw_ingredient.lower() and len(s) > len(best_match):
-            best_match = s
+    for ing in parsed_names['ingredients']:
+        if isinstance(ing, list): # Multiple names for same ingredient
+            for s in ing:
+                if found_better_match(s, raw_ingredient, best_match, parsed_names['exclude']):
+                    best_match = ing[0]
+        else:
+            if found_better_match(ing, raw_ingredient, best_match, parsed_names['exclude']):
+                best_match = ing
     
     return best_match
 
